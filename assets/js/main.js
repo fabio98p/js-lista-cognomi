63b93ab81a4aee686b9289888ecff632f4b56fd9
fabio <parisifabio98@gmail.com>
2021-03-04 16:13:17 +0100

    lista cognomi v0.1 creazione sort con i for

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
 console.log("funzia");
+
+
+var array = ["gino", "dino", "aldo"];
+
+array.push(prompt("inserisci un nome", "caio"));
+
+
+console.log(array);
+
+var x = 0
+
+var temp = ""
+
+for (let i = 0; i < array.length; i++) {
+	while(array[i-x]<array[i-x-1]){
+		temp = array[i-x-1]
+		array[i-x-1] = array[i-x]
+		array[i-x] = temp 
+		x++
+	}
+	x = 0
+}
+console.log(array)
